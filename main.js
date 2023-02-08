7c4103df828c72aa566bc4a4c6ad0b94cb3067ac
ef2m1y <shisotem01072002@gmail.com>
2023-02-08 22:51:22 +0900

    make UI

--- main.js
+++ main.js
@@ -5,4 +5,32 @@ for (let y = 0; y < 4; y++) {
         board[y][x] = y * 4 + x + 1;
     }
 }
-board[3][3] = 0; // hole
+board[3][3] = 0;
+
+// UI
+const init = () => {
+    for (let y = 0; y < 4; y++) {
+        for (let x = 0; x < 4; x++) {
+            const div = document.createElement("div");
+            document.body.appendChild(div);
+            Object.assign(div.style, {
+                position: "absolute",
+                width: "80px",
+                height: "80px",
+                top: `${y * 80}px`,
+                left: `${x * 80}px`,
+                border: "4px solid #f80",
+                backgroundColor: "#840",
+                fontSize: "50px",
+                color: "#fff",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+            });
+        }
+    }
+};
+
+window.onload = () => {
+    init();
+}
